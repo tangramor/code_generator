d9aa70d2add58a15eb136f1a4872227488753de4
王俊华 <wangjh@a-li.com.cn>
2017-08-01 16:56:35 +0800

    add prefix option

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # code_generator
 A code (coupon, security code, Anti-counterfeiting code...) generator by golang
 
-## usage
+## Usage of code_generator:
+  -len int
+        The length of code (default 18)
+  -num int
+        The total number of codes (default 100)
+  -prefix
+        If use prefix with 8 charaters (default true)
 ```
-code_generator -len=20 -num=200
+code_generator -len=20 -num=200 -prefix=false
 ```
--- code_generator.go
+++ code_generator.go
@@ -11,8 +11,9 @@ import (
 
 func main() {
 
-    length := flag.Int("len", 18, "生成码长度")
-    number := flag.Int("num", 100, "生成码数量")
+    length := flag.Int("len", 18, "The length of code")
+    number := flag.Int("num", 100, "The total number of codes")
+    usepre := flag.Bool("prefix", true, "If use prefix with 8 charaters")
     flag.Parse()
 
     chars := "23456789ABCDEFGHIJKLMNPQRSTUVWXYZ"
@@ -20,18 +21,26 @@ func main() {
 
     rand.Seed(int64(time.Now().Nanosecond()))
 
-    u4, err := uuid.NewV4()
-    if err != nil {
-        fmt.Println("error:", err)
-        return
+    leng := *length
+
+    prefix := ""
+
+    if *usepre {
+        u4, err := uuid.NewV4()
+        if err != nil {
+            fmt.Println("error:", err)
+            return
+        }
+        prefix = strings.Replace(strings.ToUpper(strings.Split(u4.String(), "-")[0]), "0", arr_chars[rand.Intn(len(arr_chars))], -1)
+        prefix = strings.Replace(prefix, "1", arr_chars[rand.Intn(len(arr_chars))], -1)
+        prefix = strings.Replace(prefix, "O", arr_chars[rand.Intn(len(arr_chars))], -1)
+
+        leng = leng - 8
     }
-    prefix := strings.Replace(strings.ToUpper(strings.Split(u4.String(), "-")[0]), "0", arr_chars[rand.Intn(len(arr_chars))], -1)
-    prefix = strings.Replace(prefix, "1", arr_chars[rand.Intn(len(arr_chars))], -1)
-    prefix = strings.Replace(prefix, "O", arr_chars[rand.Intn(len(arr_chars))], -1)
 
     for n := 0; n < *number; n ++ {
         result := ""
-        for i := 0; i < (*length - 8); i ++ {
+        for i := 0; i < leng; i ++ {
             rand.Seed(int64(time.Now().Nanosecond()))
             s := []string{result, arr_chars[rand.Intn(len(arr_chars))]}
             result = strings.Join(s, "")
